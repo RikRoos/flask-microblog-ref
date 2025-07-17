0d4efe8e5154409b300da16d394c4315afa9f9be
Rik Roos <28710055+GuzziRik@users.noreply.github.com>
2025-07-17 21:44:59 +0200

    added messages functionality

--- app/main/forms.py
+++ app/main/forms.py
@@ -44,3 +44,11 @@ class SearchForm(FlaskForm):
             kwargs['meta'] = {'csrf': False}
         super(SearchForm, self).__init__(*args, **kwargs)
 
+
+class MessageForm(FlaskForm):
+    message = TextAreaField('Message', validators=[
+        DataRequired(), Length(min=1, max=140)])
+    submit = SubmitField('Submit')
+
+# END # 
+
--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ from typing import Optional
 from flask import current_app
 import sqlalchemy as sa
 import sqlalchemy.orm as so
-import werkzeug.security as wzs
+from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 import jwt
 from app.search import add_to_index, remove_from_index, query_index
@@ -71,94 +71,102 @@ class User(UserMixin, db.Model):
     __tablename__ = "mb_users"
 
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
-    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
-    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
+                                                unique=True)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True,
+                                             unique=True)
     password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
     about_me: so.Mapped[Optional[str]] = so.mapped_column(sa.String(140))
-    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(default=lambda: datetime.now(timezone.utc))
-    # relationships otherside
-    posts: so.WriteOnlyMapped['Post'] = so.relationship(back_populates='author')
+    last_seen: so.Mapped[Optional[datetime]] = so.mapped_column(
+        default=lambda: datetime.now(timezone.utc))
+    last_message_read_time: so.Mapped[Optional[datetime]]
+
+    posts: so.WriteOnlyMapped['Post'] = so.relationship(
+        back_populates='author')
     following: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, 
-        primaryjoin=(followers.c.follower_id == id),
+        secondary=followers, primaryjoin=(followers.c.follower_id == id),
         secondaryjoin=(followers.c.followed_id == id),
-        back_populates='followers'
-    )
+        back_populates='followers')
     followers: so.WriteOnlyMapped['User'] = so.relationship(
-        secondary=followers, 
-        primaryjoin=(followers.c.followed_id == id),
+        secondary=followers, primaryjoin=(followers.c.followed_id == id),
         secondaryjoin=(followers.c.follower_id == id),
-        back_populates='following'
-    )
+        back_populates='following')
+
+    messages_sent: so.WriteOnlyMapped['Message'] = so.relationship(
+        foreign_keys='Message.sender_id', back_populates='author')
+    messages_received: so.WriteOnlyMapped['Message'] = so.relationship(
+        foreign_keys='Message.recipient_id', back_populates='recipient')
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
     def set_password(self, password):
-        self.password_hash = wzs.generate_password_hash(password)
+        self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
-        return wzs.check_password_hash(self.password_hash, password)
+        return check_password_hash(self.password_hash, password)
 
     def avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
 
     def follow(self, user):
-        """ Add argument 'user' to the group of followed users by this User."""
-        if not self.is_following(user):  # already following 'user'?
+        if not self.is_following(user):
             self.following.add(user)
 
     def unfollow(self, user):
-        """ Remove argument 'user' from the group of the followed users by this User."""
-        if self.is_following(user):  # already following 'user'?
+        if self.is_following(user):
             self.following.remove(user)
 
     def is_following(self, user):
-        """Test if this User is following argument 'user'."""
-        query = self.following.select().where (User.id == user.id)
+        query = self.following.select().where(User.id == user.id)
         return db.session.scalar(query) is not None
 
     def followers_count(self):
-        """Count the users that are following this User."""
-        query = sa.select(sa.func.count()).select_from(self.followers.select().subquery())
+        query = sa.select(sa.func.count()).select_from(
+            self.followers.select().subquery())
         return db.session.scalar(query)
 
     def following_count(self):
-        """Count the users that are followed by this User."""
-        query = sa.select(sa.func.count()).select_from(self.following.select().subquery())
+        query = sa.select(sa.func.count()).select_from(
+            self.following.select().subquery())
         return db.session.scalar(query)
 
     def following_posts(self):
         Author = so.aliased(User)
         Follower = so.aliased(User)
         return (
-                sa.select(Post)
-                .join(Post.author.of_type(Author))
-                .join(Author.followers.of_type(Follower), isouter=True)
-                .where(sa.or_(
-                    Follower.id == self.id,
-                    Author.id == self.id,
-                ))
-                .group_by(Post)
-                .order_by(Post.timestamp.desc())
-               )
+            sa.select(Post)
+            .join(Post.author.of_type(Author))
+            .join(Author.followers.of_type(Follower), isouter=True)
+            .where(sa.or_(
+                Follower.id == self.id,
+                Author.id == self.id,
+            ))
+            .group_by(Post)
+            .order_by(Post.timestamp.desc())
+        )
 
     def get_reset_password_token(self, expires_in=600):
         return jwt.encode(
-                    {'reset_password': self.id, 'exp': time() + expires_in},
-                    current_app.config['SECRET_KEY'], algorithm='HS256')
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            current_app.config['SECRET_KEY'], algorithm='HS256')
 
     @staticmethod
     def verify_reset_password_token(token):
-        #TODO: moet je de verstuurde token niet in cache zetten om later te controleren
-        # of deze wel echt verstuurd is?
         try:
             id = jwt.decode(token, current_app.config['SECRET_KEY'],
                             algorithms=['HS256'])['reset_password']
         except Exception:
-            return  #TODO:  geen logging hier ?
+            return
         return db.session.get(User, id)
 
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
+    def unread_message_count(self):
+        last_read_time = self.last_message_read_time or datetime(1900, 1, 1)
+        query = sa.select(Message).where(Message.recipient == self,
+                                         Message.timestamp > last_read_time)
+        return db.session.scalar(sa.select(sa.func.count()).select_from(
+            query.subquery()))
 
 
 class Post(SearchableMixin, db.Model):
@@ -169,7 +177,7 @@ class Post(SearchableMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
     timestamp: so.Mapped[datetime] = so.mapped_column(index=True, 
-                                                      default=lambda: datetime.now(timezone.utc)) 
+                                         default=lambda: datetime.now(timezone.utc)) 
     user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), index=True)
     language: so.Mapped[Optional[str]] = so.mapped_column(sa.String(5))  # detected language
     # relationships otherside
@@ -177,6 +185,30 @@ class Post(SearchableMixin, db.Model):
 
     def __repr__(self):
         return '<Post {}>'.format(self.body)
+
+
+class Message(db.Model):
+
+    __tablename__ = "mb_messages"
+
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    sender_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), 
+                                                 index=True)
+    recipient_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey(User.id), 
+                                                    index=True)
+    body: so.Mapped[str] = so.mapped_column(sa.String(140))
+    timestamp: so.Mapped[datetime] = so.mapped_column(
+        index=True, default=lambda: datetime.now(timezone.utc))
+
+    author: so.Mapped[User] = so.relationship(
+        foreign_keys='Message.sender_id',
+        back_populates='messages_sent')
+    recipient: so.Mapped[User] = so.relationship(
+        foreign_keys='Message.recipient_id',
+        back_populates='messages_received')
+
+    def __repr__(self):
+        return '<Message {}>'.format(self.body)
 
 
 @login.user_loader
